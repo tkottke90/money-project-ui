c3395798b9ecc4852c4ba09a7c6de314861e4b59
Thomas Kottke <t.kottke90@gmail.com>
2019-10-25 22:28:41 -0500

    feat(proxy): Add redirect to angular app

--- proxy/index.js
+++ proxy/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
-const api = process.env.API_URL;
-const ui = 'localhost:4200';
-
 var app = express();
 
 app.use('/', proxy({
-	target: ui,
-	changeOrigin: true
+	target: `http://localhost:${process.env.TARGET_PORT}`,
+	ws: true
+}));
+
+app.use('/api', proxy({
+	target: process.env.API_URL,
+	changeOrigin: true,
+	ws: true
 }));
 
 app.listen(process.env.PROXY_PORT, () => {
-	console.info('Proxy started on Port 3000')
+	console.info(`Proxy started on Port ${process.env.PROXY_PORT}`)
 });
