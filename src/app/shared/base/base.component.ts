95e059a96202fc5116ae0dd3e4fd14092c80de72
Thomas Kottke <t.kottke90@gmail.com>
2019-10-25 22:30:00 -0500

    feat: Add app base component

---
+++ src/app/shared/base/base.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-base',
+  templateUrl: './base.component.html',
+  styleUrls: ['./base.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BaseComponent implements OnInit {
+
+  @Input() title: string = 'Money App';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
