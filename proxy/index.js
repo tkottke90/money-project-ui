2abe890fdef298d6506c1945d289f39ff3c6cfc8
Thomas Kottke <t.kottke90@gmail.com>
2019-10-28 19:08:01 -0500

    refactor:Update api proxy route w/ redirect ## Git Commit Template v1.0 ## Author: Thomas Kottke <admin@tdkottke.com> ## Template supports the logging of information related to the ##    ticket based on multiple git commit template philosophies
    # Files committed:
      - proxy/index.js
    
    # Ticketing System Information
      # ticket: <tracking ticket>
    
    # Commit Summery:
    # Why is the change necessary?
    Ensures that api routes are separate from ui routes
    
    # How does it address the issue?
        Add pathRewrite param to api configs
    
    # What side effects does this change have?
        N/A

--- proxy/index.js
+++ proxy/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
+const routes = require('./routes');
+
 var app = express();
 
-app.use('/', proxy({
-	target: `http://localhost:${process.env.TARGET_PORT}`,
+// routes(app);
+
+app.use('/api', proxy({
+	pathRewrite: { '^/api': '' },
+	target: process.env.API_URL,
+	changeOrigin: true,
 	ws: true
 }));
 
-app.use('/api', proxy({
-	target: process.env.API_URL,
-	changeOrigin: true,
+app.use('/', proxy({
+	target: `http://localhost:${process.env.TARGET_PORT}`,
 	ws: true
 }));
 
