2d778bc88c54e95d0569e137416fa572eacf5711
Thomas Kottke <t.kottke90@gmail.com>
2019-11-01 22:45:39 -0500

    refactor: Add MatInput to login module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,6 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 // Angular Material
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
@@ -23,6 +24,7 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatInputModule,
     MatSnackBarModule
   ]
 })
