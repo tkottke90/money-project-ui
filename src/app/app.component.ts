b94f484c3c72cde0d9237676363e367840aea404
Thomas Kottke <t.kottke90@gmail.com>
2019-11-01 20:55:56 -0500

    refactor(app.component): RM test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UsersService } from './services/users/users.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'money-ui';
 
-  constructor() { }
+  constructor() {}
 }
