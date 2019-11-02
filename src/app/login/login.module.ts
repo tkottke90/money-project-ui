3456cb86b0605bab72d8759c20a075120f32dce6
Thomas Kottke <t.kottke90@gmail.com>
2019-11-01 21:07:00 -0500

    feat: Add Angular Material Modules to Login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login.routing';
 
+// Angular Material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+// Components
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    // material
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSnackBarModule
   ]
 })
 export class LoginModule { }
