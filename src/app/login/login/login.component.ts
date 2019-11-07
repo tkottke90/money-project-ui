757fb188d7c3d167d510983a3b250d4b47756779
Thomas Kottke <t.kottke90@gmail.com>
2019-11-06 22:08:49 -0600

    feat: Add errors to login logic

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { UsersService } from '../../services/users/users.service';
+import { Router } from '@angular/router';
+
+const ErrorMessages = {
+  required: 'This fields is required',
+  email: 'Invalid Email'
+};
 
 @Component({
   selector: 'app-login',
@@ -10,9 +17,13 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  loadingState = false;
 
   constructor(
-    private fb: FormBuilder
+    private cdr: ChangeDetectorRef,
+    private fb: FormBuilder,
+    private userService: UsersService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -24,6 +35,34 @@ export class LoginComponent implements OnInit {
       email: ['', [ Validators.required, Validators.email ]],
       password: [ '', [ Validators.required ] ]
     });
+  }
+
+  getErrors(field) {
+    const errors = this.loginForm.get(field).errors;
+    const errorList = Object.keys(errors);
+
+    if (errorList.length > 0) {
+      return ErrorMessages[errorList[0]];
+    }
+    return '';
+  }
+
+  async login() {
+    if (this.loginForm.valid) {
+      const result = await this.userService.login(this.loginForm.value.email, this.loginForm.value.password);
+
+      if (result) {
+        console.log('success!');
+        this.router.navigate(['/']);
+      } else {
+        console.log('failed');
+        this.loginForm.markAllAsTouched();
+        this.cdr.markForCheck();
+      }
+    
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
   }
 
 }
